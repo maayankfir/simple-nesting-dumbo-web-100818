8f457471c2c9ba66ec7af266467afb1078669744
Maayan Kfir <mayankfir@gmail.com>
2018-09-26 20:39:54 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -55,14 +55,10 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+		 programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
 
 	programmer_hash =
  		{
